ac4c20a1af3ece038081599d668bc47b6f67371b
Denis Shirokov <de.sh.vit@gmail.com>
2022-03-05 22:22:37 +0300

    add tezos account wallet service

---
+++ src/main/solidity/debot/tezos/ITezosAccountService.sol
@@ -0,0 +1,27 @@
+pragma ton-solidity >= 0.53.0;
+
+
+library TezosLibrary {
+    struct Account {
+        optional(string) addr;
+        optional(uint32) signBoxHandle;
+    }
+}
+
+interface ITezosAccountServiceMenuItem {
+    struct MenuItem {
+        string title;
+        string description;
+        uint order;
+    }
+
+    function menuItem(TezosLibrary.Account request) external responsible returns(MenuItem);
+}
+
+interface ITezosAccountServiceCall {
+    function call(TezosLibrary.Account request) external responsible returns(TezosLibrary.Account);
+}
+
+interface ITezosAccountServiceRun {
+    function run(TezosLibrary.Account request) external;
+}
